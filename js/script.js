0a2ad256c33d663825355dccec8054db461305c7
miashef <90767039+miashef@users.noreply.github.com>
2022-05-29 09:06:16 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+import {setOnClickBlur} from './utils/blur.js';
+import {slider} from './modules/slider.js';
+
+const sliderRoots = document.querySelectorAll(`.slider`);
+
+sliderRoots.forEach((root) => {
+  slider(root);
+});
+
+setOnClickBlur();
